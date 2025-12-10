4d30671f0705b3a9f096043e370219f031a31c05
Brandon Hearley <brandon.l.hearley@nasa.gov>
2025-12-10 16:00:52 -0500

    safe writeRUC

--- RUC_Generator/Write/WriteRUC.py
+++ RUC_Generator/Write/WriteRUC.py
@@ -1,47 +1,62 @@
-def WriteRUC(mask):
+def WriteRUC(mask, max_len=400):
     """
-    Generate the *RUC portion of a NASMAT input deck to *.txt
+    Generate the *RUC portion of a NASMAT input deck ensuring each line
+    does not exceed max_len characters. Lines longer than max_len are
+    split using '&' continuation.
 
     Arguments:
         mask        2D array    integer array defining the microstructure
+        max_len     int         maximum allowed line length before splitting
 
     Outputs:
-        ruc_data    str         str for streamlit to write to txt
+        ruc_data    str         formatted input section
     """
 
+    def append_safe(ruc, text, max_len=400):
+        """Append text to ruc safely, inserting '&' and newlines if needed."""
+        lines = ruc.split("\n")
+        current = lines[-1]  # last line being built
+
+        for ch in text:
+            if len(current) >= max_len:
+                # Break line with continuation
+                lines[-1] = current + "&"
+                lines.append("")  # Start new physical line
+                current = ""
+
+            current += ch
+
+        lines[-1] = current
+        return "\n".join(lines)
+
     # Begin writing the text data
-    ruc_data = '*RUC\n'
-    ruc_data = ruc_data + f" MOD={202} ARCHID={99} \n"
+    ruc = "*RUC\n"
+    ruc += f" MOD={202} ARCHID={99}\n"
 
     # Get Subcell Counts
-    NB =  len(mask)
-    NG =  len(mask[0]) 
-    ruc_data = ruc_data + f" NB={NB} NG={NG} \n"
+    NB = len(mask)
+    NG = len(mask[0])
+    ruc += f" NB={NB} NG={NG}\n"
 
     # Write H
-    ruc_data = ruc_data + ' H='
+    ruc += " H="
     for i in range(NB):
-        ruc_data = ruc_data + '1'
-        if i == NB - 1:
-            ruc_data = ruc_data + '\n'
-        else:
-            ruc_data = ruc_data + ','
+        ruc = append_safe(ruc, "1" + ("," if i != NB - 1 else "\n"), max_len)
 
     # Write L
-    ruc_data = ruc_data + ' L='
+    ruc += " L="
     for i in range(NG):
-        ruc_data = ruc_data + '1'
-        if i == NG - 1:
-            ruc_data = ruc_data + '\n'
-        else:
-            ruc_data = ruc_data + ','
+        ruc = append_safe(ruc, "1" + ("," if i != NG - 1 else "\n"), max_len)
 
+    # Write SM rows
     for i in range(NB):
-        ruc_data = ruc_data + " SM="
+        ruc += " SM="
         for j in range(NG):
-            ruc_data = ruc_data + f"{int(mask[i][j])}"
+            entry = f"{int(mask[i][j])}"
             if j != NG - 1:
-                ruc_data = ruc_data + ","
-        ruc_data = ruc_data + "\n"
+                entry += ","
+            else:
+                entry += "\n"
+            ruc = append_safe(ruc, entry, max_len)
 
-    return ruc_data
+    return ruc
